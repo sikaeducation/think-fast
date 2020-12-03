1eab4dc64be633e564b3ac69a6bf67978cade17c
Kyle Coberly <kyle.coberly@gmail.com>
2020-12-03 10:50:57 -0700

    Removes feedback from API get question spec

--- features/tests/specs/questions-api.spec.js
+++ features/tests/specs/questions-api.spec.js
@@ -3,8 +3,8 @@ const { request, server, route } = cy
 describe("/question", () => {
   it("generates a question", () => {
     const question = {
-      correctFeedback: "That's PascalCase, two words, and uses the App prefix.",
-      incorrectFeedback: "That's PascalCase, two words, and uses the App prefix.",
+      // correctFeedback: "That's PascalCase, two words, and uses the App prefix.",
+      // incorrectFeedback: "That's PascalCase, two words, and uses the App prefix.",
       promptText: '<p>According to the official <a href="https://v3.vuejs.org/style-guide">Vue style guide</a>, is this a good name for Vue component?</p>',
       stemText: 'AppIndex.vue',
     }
