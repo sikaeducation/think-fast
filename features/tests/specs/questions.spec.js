ddcfdccc55c3b5b88f0dea24db8dd106505ad830
Kyle Coberly <kyle.coberly@gmail.com>
2020-12-05 18:42:05 -0700

    refactoring tests

--- features/tests/specs/questions.spec.js
+++ features/tests/specs/questions.spec.js
@@ -5,7 +5,7 @@ describe("Networking loading", () => {
     server()
     route({
       method: "POST",
-      url: "https://thinkfast-api.sikaeducation.com/get-next-question",
+      url: "https://thinkfast-api.sikaeducation.com/get-question",
       response: { question: { promptText: "anything" }},
       delay: 1000,
     }).as("getQuestion")
@@ -16,14 +16,16 @@ describe("Networking loading", () => {
 describe("Taking quizzes", () => {
   beforeEach(() => {
     const question = {
-      correctFeedback: "That's PascalCase, two words, and uses the App prefix.",
-      incorrectFeedback: "That's PascalCase, two words, and uses the App prefix.",
       promptText: '<p>According to the official <a href="https://v3.vuejs.org/style-guide">Vue style guide</a>, is this a good name for Vue component?</p>',
       stemText: 'AppIndex.vue',
-      isCorrect: false,
+    }
+    const answer = {
+      isCorrect: true,
+      feedback: 'feedback',
     }
     server()
-    route("POST", "https://thinkfast-api.sikaeducation.com/get-next-question", { question }).as("getQuestion")
+    route("POST", "https://thinkfast-api.sikaeducation.com/get-question", { question }).as("getQuestion")
+    route("POST", "https://thinkfast-api.sikaeducation.com/evaluate-answer", { answer }).as("answerQuestion")
     visit("/")
     wait("@getQuestion")
     the("loading-indicator").should("not.exist")
@@ -33,29 +35,39 @@ describe("Taking quizzes", () => {
       the("prompt").contains("AppIndex.vue")
     })
     it("shows multiple questions", () => {
-      const question = {
-        correctFeedback: "That's PascalCase, two words, and uses the App prefix.",
-        incorrectFeedback: "That's PascalCase, two words, and uses the App prefix.",
+      let question = {
         promptText: '<p>According to the official <a href="https://v3.vuejs.org/style-guide">Vue style guide</a>, is this a good name for Vue component?</p>',
         stemText: 'app-index.vue',
-        isCorrect: false,
       }
-      route("POST", "https://thinkfast-api.sikaeducation.com/get-next-question", { question }).as("getQuestion")
+      route("POST", "https://thinkfast-api.sikaeducation.com/get-question", { question }).as("getQuestion")
       the("prompt").contains("AppIndex.vue")
       clickThe("yes-button")
+      wait("@answerQuestion")
+      question = {
+        promptText: '<p>According to the official <a href="https://v3.vuejs.org/style-guide">Vue style guide</a>, is this a good name for Vue component?</p>',
+        stemText: 'the-component.vue',
+      }
+      route("POST", "https://thinkfast-api.sikaeducation.com/get-question", { question }).as("getQuestion")
       clickThe("next-button")
       wait("@getQuestion")
-      the("prompt").contains("app-index.vue")
+      the("prompt").contains("the-component.vue")
     })
   })
 
   describe("Answering questions", () => {
     it("can answer a question correctly", () => {
       clickThe("yes-button")
+      wait("@answerQuestion")
       the("is-correct-message").contains("Correct!")
     })
     it("can answer a question incorrectly", () => {
+      const answer = {
+        isCorrect: false,
+        feedback: 'feedback',
+      }
+      route("POST", "https://thinkfast-api.sikaeducation.com/evaluate-answer", { answer }).as("answerQuestion")
       clickThe("no-button")
+      wait("@answerQuestion")
       the("is-correct-message").contains("Incorrect.")
     })
   })
@@ -64,19 +76,33 @@ describe("Taking quizzes", () => {
     beforeEach(() => {
       the("streak").should("not.exist")
       clickThe("yes-button")
+      wait("@answerQuestion")
       clickThe("next-button")
       the("streak").should("not.exist")
       clickThe("yes-button")
+      wait("@answerQuestion")
       clickThe("next-button")
     })
     it("displays a streak if multiple questions are answered correctly", () => {
       the("streak").contains("2")
       clickThe("yes-button")
       clickThe("next-button")
+      const question = {
+        promptText: '<p>According to the official <a href="https://v3.vuejs.org/style-guide">Vue style guide</a>, is this a good name for Vue component?</p>',
+        stemText: 'the-component.vue',
+      }
+      route("POST", "https://thinkfast-api.sikaeducation.com/get-question", { question }).as("getQuestion")
+      wait("@getQuestion")
       the("streak").contains("3")
     })
     it("stops display a streak if it's broken", () => {
+      const answer = {
+        isCorrect: false,
+        feedback: 'feedback',
+      }
+      route("POST", "https://thinkfast-api.sikaeducation.com/evaluate-answer", { answer }).as("answerQuestion")
       clickThe("no-button")
+      wait("@answerQuestion")
       the("streak").should("not.exist")
     })
   })
